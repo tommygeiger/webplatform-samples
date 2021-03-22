5ba4a76e097a0bae0fe0c3243aeb9b867c521f59
Thomas Geiger <tommyhawk21@gmail.com>
2021-03-22 17:51:43 -0400

    Speaking rate demo working, index page

---
+++ script.js
@@ -0,0 +1,245 @@
+var langs =
+    [['Afrikaans', ['af-ZA']],
+    ['Bahasa Indonesia', ['id-ID']],
+    ['Bahasa Melayu', ['ms-MY']],
+    ['Català', ['ca-ES']],
+    ['Čeština', ['cs-CZ']],
+    ['Deutsch', ['de-DE']],
+    ['English', ['en-AU', 'Australia'],
+        ['en-CA', 'Canada'],
+        ['en-IN', 'India'],
+        ['en-NZ', 'New Zealand'],
+        ['en-ZA', 'South Africa'],
+        ['en-GB', 'United Kingdom'],
+        ['en-US', 'United States']],
+    ['Español', ['es-AR', 'Argentina'],
+        ['es-BO', 'Bolivia'],
+        ['es-CL', 'Chile'],
+        ['es-CO', 'Colombia'],
+        ['es-CR', 'Costa Rica'],
+        ['es-EC', 'Ecuador'],
+        ['es-SV', 'El Salvador'],
+        ['es-ES', 'España'],
+        ['es-US', 'Estados Unidos'],
+        ['es-GT', 'Guatemala'],
+        ['es-HN', 'Honduras'],
+        ['es-MX', 'México'],
+        ['es-NI', 'Nicaragua'],
+        ['es-PA', 'Panamá'],
+        ['es-PY', 'Paraguay'],
+        ['es-PE', 'Perú'],
+        ['es-PR', 'Puerto Rico'],
+        ['es-DO', 'República Dominicana'],
+        ['es-UY', 'Uruguay'],
+        ['es-VE', 'Venezuela']],
+    ['Euskara', ['eu-ES']],
+    ['Français', ['fr-FR']],
+    ['Galego', ['gl-ES']],
+    ['Hrvatski', ['hr_HR']],
+    ['IsiZulu', ['zu-ZA']],
+    ['Íslenska', ['is-IS']],
+    ['Italiano', ['it-IT', 'Italia'],
+        ['it-CH', 'Svizzera']],
+    ['Magyar', ['hu-HU']],
+    ['Nederlands', ['nl-NL']],
+    ['Norsk bokmål', ['nb-NO']],
+    ['Polski', ['pl-PL']],
+    ['Português', ['pt-BR', 'Brasil'],
+        ['pt-PT', 'Portugal']],
+    ['Română', ['ro-RO']],
+    ['Slovenčina', ['sk-SK']],
+    ['Suomi', ['fi-FI']],
+    ['Svenska', ['sv-SE']],
+    ['Türkçe', ['tr-TR']],
+    ['български', ['bg-BG']],
+    ['Pусский', ['ru-RU']],
+    ['Српски', ['sr-RS']],
+    ['한국어', ['ko-KR']],
+    ['中文', ['cmn-Hans-CN', '普通话 (中国大陆)'],
+        ['cmn-Hans-HK', '普通话 (香港)'],
+        ['cmn-Hant-TW', '中文 (台灣)'],
+        ['yue-Hant-HK', '粵語 (香港)']],
+    ['日本語', ['ja-JP']],
+    ['Lingua latīna', ['la']]];
+
+for (var i = 0; i < langs.length; i++) {
+    select_language.options[i] = new Option(langs[i][0], i);
+}
+select_language.selectedIndex = 6;
+updateCountry();
+select_dialect.selectedIndex = 6;
+showInfo('info_start');
+
+function updateCountry() {
+    for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+        select_dialect.remove(i);
+    }
+    var list = langs[select_language.selectedIndex];
+    for (var i = 1; i < list.length; i++) {
+        select_dialect.options.add(new Option(list[i][1], list[i][0]));
+    }
+    select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+}
+
+var create_email = false;
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;
+
+// Vars
+var wpm_interval;
+var wc_runningsum = 0;
+var wc_active = 0;
+var wc_list = [];
+
+
+if (!('webkitSpeechRecognition' in window)) {
+    upgrade();
+
+} else {
+    start_button.style.display = 'inline-block';
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = function () {
+        recognizing = true;
+        showInfo('info_speak_now');
+        start_img.src = 'assets/mic-animate.gif';
+
+        //Set Interval
+        wpm_interval = setInterval(do_wpm, 1000);
+    };
+
+    recognition.onerror = function (event) {
+        if (event.error == 'no-speech') {
+            start_img.src = 'assets/mic.gif';
+            showInfo('info_no_speech');
+            ignore_onend = true;
+        }
+        if (event.error == 'audio-capture') {
+            start_img.src = 'assets/mic.gif';
+            showInfo('info_no_microphone');
+            ignore_onend = true;
+        }
+        if (event.error == 'not-allowed') {
+            if (event.timeStamp - start_timestamp < 100) {
+                showInfo('info_blocked');
+            } else {
+                showInfo('info_denied');
+            }
+            ignore_onend = true;
+        }
+    };
+
+    recognition.onend = function () {
+        recognizing = false;
+
+        //Clear Interval
+        clearInterval(wpm_interval);
+
+        if (ignore_onend) {
+            return;
+        }
+        start_img.src = 'assets/mic.gif';
+        if (!final_transcript) {
+            showInfo('info_start');
+            return;
+        }
+        showInfo('');
+    };
+
+    recognition.onresult = function (event) {
+        var interim_transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+
+                //Update running sum
+                final_transcript += event.results[i][0].transcript;                
+                wc_runningsum = final_transcript.match(/\S+/g).length;
+            } else {
+
+                //Update active word count
+                interim_transcript += event.results[i][0].transcript; 
+                wc_active = interim_transcript.match(/\S+/g).length + wc_runningsum;
+            }
+        }
+        final_transcript = capitalize(final_transcript);
+        final_span.innerHTML = linebreak(final_transcript);
+        interim_span.innerHTML = linebreak(interim_transcript);
+    };
+}
+
+
+//Called every 1 seconds
+function do_wpm() {
+
+    //Keep list to N elements
+    wc_list.push(wc_active);
+    if (wc_list.length > 16) {
+        wc_list.shift();
+    }
+
+    //Get range of list, scale to one minute
+    var wpm = (wc_list[wc_list.length-1] - wc_list[0]) * (60/(wc_list.length-1));
+    document.getElementById("wpm").innerHTML = wpm.toFixed(0);
+
+    //Print feedback
+    if (wpm < 133 ) {
+        document.getElementById("feedback").innerHTML = "Talk a little faster.";
+    } else if (wpm > 188 ) {
+        document.getElementById("feedback").innerHTML = "Talk a little slower.";
+    } else {
+        document.getElementById("feedback").innerHTML = "Great job!";
+    }
+
+}
+
+
+
+function upgrade() {
+    start_button.style.visibility = 'hidden';
+    showInfo('info_upgrade');
+}
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+function capitalize(s) {
+    return s.replace(first_char, function (m) { return m.toUpperCase(); });
+}
+
+function startButton(event) {
+    if (recognizing) {
+        recognition.stop();
+        return;
+    }
+    final_transcript = '';
+    recognition.lang = select_dialect.value;
+    recognition.start();
+    ignore_onend = false;
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+    start_img.src = 'assets/mic-slash.gif';
+    showInfo('info_allow');
+    showButtons('none');
+    start_timestamp = event.timeStamp;
+}
+
+function showInfo(s) {
+    if (s) {
+        for (var child = info.firstChild; child; child = child.nextSibling) {
+            if (child.style) {
+                child.style.display = child.id == s ? 'inline' : 'none';
+            }
+        }
+        info.style.visibility = 'visible';
+    } else {
+        info.style.visibility = 'hidden';
+    }
+}
